52fb6652fc3046de604a5912a39a13fe88e0a317
guanxiux <guanxx@connect.hku.hk>
2023-05-15 01:40:49 +0800

    auto shell script

---
+++ config_my_env.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install git zsh curl wget zsh-autosuggestions zsh-syntax-highlighting
+mkdir -p ~/.zshpkg
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zshpkg/powerlevel10k
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.zshpkg/zsh-history-substring-search
+
+# git clone --depth=1 https://github.com/guanxiux/myzsh.git ~/.zshpkg/myzsh
+cp ~/.zshpkg/myzsh/.zshrc .
+cp ~/.zshpkg/myzsh/.p10k.zsh .
+
+cd
+git clone --depth=1 https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
