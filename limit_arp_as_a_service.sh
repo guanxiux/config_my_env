564a3d9ee4a6f35b729c2013b9e5e5a1bf397bb6
guanxx <guanxx@connect.hku.hk>
2023-05-22 11:29:15 +0800

    fix authority

--- limit_arp_as_a_service.sh
+++ limit_arp_as_a_service.sh
@@ -6,7 +6,6 @@ echo "$(whoami)"
 echo "Registering limit_arp as a service"
 
 echo -e "[Unit]\nDescription=Limiting transmission rate of ARP packages using tc\nAfter=network.target\n\n[Service]\nType=oneshot\nExecStart=/opt/limit_arp.sh\nExecReload=/opt/limit_arp.sh\nRemainAfterExit=yes\n\n[Install]\nWantedBy=multi-user.target\n" > /usr/lib/systemd/system/limit_arp.service
-
 cp limit_arp.sh /opt/limit_arp.sh
 chmod a+x /opt/limit_arp.sh
 
