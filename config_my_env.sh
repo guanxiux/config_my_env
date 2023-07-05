a81151b7a0bd656b0227f165d3dfebc283f78ec7
guanxx <guanxx@connect.hku.hk>
2023-07-06 02:20:25 +0800

    xclip

--- config_my_env.sh
+++ config_my_env.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y git zsh curl wget zsh-autosuggestions zsh-syntax-highlighting tmux
+sudo apt install -y git zsh curl wget zsh-autosuggestions zsh-syntax-highlighting tmux xclip
 mkdir -p ~/.zshpkg
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zshpkg/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git ~/.zshpkg/zsh-history-substring-search
