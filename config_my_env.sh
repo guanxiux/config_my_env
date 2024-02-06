cb24964d1adead56c6379d9c76cc599d3ca38b1c
guanxx <guanxx@connect.hku.hk>
2024-02-06 18:07:07 +0800

    fix

--- config_my_env.sh
+++ config_my_env.sh
@@ -1,8 +1,8 @@
-sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt update
-sudo apt install -y git zsh curl wget tmux xclip helix
+sudo apt install -y git zsh curl wget tmux xclip software-properties-common
+sudo add-apt-repository ppa:maveonair/helix-editor && sudo apt update && sudo apt install -y helix
 mkdir -p ~/.zshpkg
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.zshpkg/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zshpkg/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zshpkg/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git ~/.zshpkg/zsh-history-substring-search
